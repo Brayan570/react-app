093a3115aff3fa7606d3f25bec9c915d5645d3f0
Brayan Gomez <gomezbrayanyesid78@gmail.com>
2025-05-19 12:03:17 -0500

    Updates

---
+++ precache-manifest.0d62b97cb563f78eb8a7289261d1e815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9890d7210246f952548cbb794675c5f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "ac7fc26a1ba0609520c6",
+    "url": "/react-app/static/css/main.3bbe37bf.chunk.css"
+  },
+  {
+    "revision": "da8b8ee81106c8324f14",
+    "url": "/react-app/static/js/2.4519393a.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/react-app/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "ac7fc26a1ba0609520c6",
+    "url": "/react-app/static/js/main.2c3ea925.chunk.js"
+  },
+  {
+    "revision": "58c24cef44e3d1a9b0a4",
+    "url": "/react-app/static/js/runtime~main.d74c3b53.js"
+  }
+]);
