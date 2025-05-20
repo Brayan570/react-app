5289b1ee552df53a99a17b936b16f0373d22a0a0
Brayan Gomez <gomezbrayanyesid78@gmail.com>
2025-05-20 12:47:12 -0500

    Correccion de letras en la en el About y Projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     <section id="sobre-mi" className="about-section">
       <h3 className="about-title">Sobre mí</h3>
       <p className="about-text">
-        Soy desarrollador Frontend con experiencias solidad en HTML5, CSS3, JavaScript, PHP, SQL, con conocimientos basicos en React y Node.js. Me apasiona crear aplicaciones web atractivas y funcionales, siempre buscando mejorar mis habilidades y aprender nuevas tecnologías.
+        Soy desarrollador Frontend con experiencias solidas en HTML5, CSS3, JavaScript, PHP, SQL, con conocimientos basicos en React y Node.js. Me apasiona crear aplicaciones web atractivas y funcionales, siempre buscando mejorar mis habilidades y aprender nuevas tecnologías.
         <br />
         Experiencia en la creacion y mantenimiento de sistemas creados en WordPress y asi mismo escalamiento de proyectos de e-commerce, como lo es Woocommerce.
       </p>
