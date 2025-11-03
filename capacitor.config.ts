1c038f9dc2d81ceabb86310c11302561a8239728
Brayan570 <gomezbrayanyesid78@gmail.com>
2025-11-02 21:25:45 -0500

    Updates

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.react.app',
-  appName: 'my-react-app',
-  webDir: 'build'
-};
-
-export default config;
---
+++ precache-manifest.a9a324e8c18c4698844b6f8eba14db74.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b7de5060088d1d1c368e910fdc9b55",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9d8b0353479afec93f4c",
+    "url": "/react-app/static/css/main.cb8fc601.chunk.css"
+  },
+  {
+    "revision": "86a103ff19d10fcfbe6a",
+    "url": "/react-app/static/js/2.ece5a136.chunk.js"
+  },
+  {
+    "revision": "5099652deffd979d970e",
+    "url": "/react-app/static/js/3.d5f86460.chunk.js"
+  },
+  {
+    "revision": "9d8b0353479afec93f4c",
+    "url": "/react-app/static/js/main.97bf35a2.chunk.js"
+  },
+  {
+    "revision": "284af93356e22e186bac",
+    "url": "/react-app/static/js/runtime~main.35393859.js"
+  },
+  {
+    "revision": "666c28f1af4645298a77a48e071393f0",
+    "url": "/react-app/static/media/hero.666c28f1.jpg"
+  },
+  {
+    "revision": "b0d084b3b26c9cacf2f7be2e0042a975",
+    "url": "/react-app/static/media/kelly.b0d084b3.jpg"
+  },
+  {
+    "revision": "a4db77784d9a768c4ec7e516ce1049a2",
+    "url": "/react-app/static/media/meta.a4db7778.png"
+  },
+  {
+    "revision": "68e244a5efa512e1910bfbb82739238b",
+    "url": "/react-app/static/media/tiktok.68e244a5.png"
+  },
+  {
+    "revision": "d266b6114ffcaf2dfb0b7cca64d4968b",
+    "url": "/react-app/static/media/whatsapp.d266b611.png"
+  },
+  {
+    "revision": "cba1634f435d5290129aefc666ad5532",
+    "url": "/react-app/static/media/yesid.cba1634f.jpg"
+  }
+]);
