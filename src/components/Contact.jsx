98e3390b84d7902e7d72211e405f781e2f28991f
Brayan570 <gomezbrayanyesid78@gmail.com>
2025-11-06 17:45:13 -0500

    refactory: cambio de correo y numero en componente contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,7 +36,7 @@ export default function Contact() {
       icon: "M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z",
       label: "Email",
       value: "gomezbrayanyesid78@gmail.com",
-      href: "gomezbrayanyesid78@gmail.com",
+      href: "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dgmail%2Bemail%26oq%3Dgma%26pf%3Dop%26sourceid%3Dchrome%26ie%3DUTF-8&ec=futura_srp_og_si_72236_p&hl=es-419&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-1491280510%3A1762469046896990",
     },
     {
       icon: "M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z",
@@ -62,7 +62,7 @@ export default function Contact() {
     {
       name: "LinkedIn",
       icon: "M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z",
-      href: "www.linkedin.com/in/brayan-gómez-5319231b7",
+      href: "https://www.linkedin.com/in/brayan-g%C3%B3mez-5319231b7/",
       color: "linkedin",
     },
     {
